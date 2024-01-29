eb73493c24fb6e7a95f2e652fdf380d77020428b
JLJL <juanluisjaramillolopez@gmail.com>
2024-01-29 14:22:45 -0600

    configuracion base del contexto global y reducer para el logoe de usuarios

--- src/context/ReducerContext.jsx
+++ src/context/ReducerContext.jsx
@@ -1,9 +1,22 @@
 //este componentes solo tendra la logica de cada cazo en especifico es decir si esta o no logeado el usuario y el acceso
 
-const ReducerContext = () => {
-  return (
-    <div>ReducerContext</div>
-  )
+const ReducerContext = (state, action) => {
+  //mediante el estado y la accion se le dira en que caso deve acceder o no el usuario
+  switch (action.type) {
+    case 'LOGIN':
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload
+      }
+  
+    case 'LOGOUT':
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null
+      }
+  }
 }
 
 export default ReducerContext
