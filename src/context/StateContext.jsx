b2a91d17c0e6e52c63474af5f9e91882d286032b
JLJL <juanluisjaramillolopez@gmail.com>
2024-01-30 17:32:46 -0600

    creacion de las funciones de registro y login asi como el acceso a home mediante el uso del contexto de la app

--- src/context/StateContext.jsx
+++ src/context/StateContext.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types'
 import { UserContext } from './UserContext'
 import ReducerContext from './ReducerContext'
 import { useReducer } from 'react'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { auth } from '../firebase/firebaseConfig'
 //?el estado resive la propiedad childrem para compartir datos en toda la application
 
 const StateContext = ( { children } ) => {
@@ -11,13 +13,24 @@ const StateContext = ( { children } ) => {
     user: null,
   }
 
+  //?creasmo una funcion para la creacion de usuarios en el registro
+  const registerUser =(email, password)=>//resive los parametros del registro
+     createUserWithEmailAndPassword(auth, email, password)
+
+  //?creamos otre funcion para el login
+  const signIn = async (emai, password)=> signInWithEmailAndPassword(auth, emai, password)
+
+  //?creamos las funciones que validen la autentiaccion como el acceso a home y cualquier apartado de la ruta
+  const isAuth =()=> dispatch( { type: 'LOGIN' } )
+  const notAuth =()=> dispatch( { type: 'LOGOUT' } )
+
   //*useReducer lo usamos para la manipulacion del estado en el que se encuentra el logeo
   const [state, dispatch] = useReducer(ReducerContext, initialState)
 
   //todo lo que contenga la propiedad de value se compartira con toda la app
   return (
     <UserContext.Provider value={{
-      state, dispatch
+      state, dispatch, registerUser, signIn, isAuth, notAuth
     }}>
       { children }
     </UserContext.Provider>
